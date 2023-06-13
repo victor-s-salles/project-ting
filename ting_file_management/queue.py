39e45f1793dc3ea8c4ac922f9f077072637fce10
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-13 17:23:00 -0300

    Feat: Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            raise IndexError("Fila vazia, não é possivel remover elementos")
+
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not 0 <= index < len(self._data):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
