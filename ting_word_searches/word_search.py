7d8318917a2ee947629866adefce1337c11557f4
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-14 15:39:18 -0300

    Feat: Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        search = instance.search(i)
+        data = {
+            "palavra": word,
+            "arquivo": search["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for text in search["linhas_do_arquivo"]:
+            if word.lower() in text.lower():
+                data["ocorrencias"].append(
+                    {"linha": search["linhas_do_arquivo"].index(text) + 1}
+                )
+        if len(data["ocorrencias"]) > 0:
+            result.append(data)
+    return result
 
 
 def search_by_word(word, instance):
